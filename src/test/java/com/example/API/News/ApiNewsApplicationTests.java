a34f7d63daa53ef65f8fc9e6e51497b86894339b
Camila Artigas <camila_pra@estudante.sesisenai.org.br>
2023-11-26 18:22:29 -0300

    exercicio01

---
+++ src/test/java/com/example/API/News/ApiNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.API.News;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
